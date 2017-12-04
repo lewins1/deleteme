291a48dc7cf14e3e1d067f4392e53fc269e4859a
Shawn Lewin <lewins@ad.nmc.edu>
2017-12-04 08:10:46 -0500

    Add project files.

---
+++ Deleteme/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deleteme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Henlo Werld.");
+            Console.ReadKey();
+        }
+    }
+}
